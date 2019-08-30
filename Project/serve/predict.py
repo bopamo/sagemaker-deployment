8a6dff27188a708a80ebd401771ef28398882896
Bopamo Osaisai <bopamo@gmail.com>
2019-08-30 22:36:23 +0000

    Completed Sentiment Analysis project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,11 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    
+    review_data, review_length = convert_and_pad(model.word_dict, review_to_words(input_data))
 
-    data_X = None
-    data_len = None
+    data_X = review_data
+    data_len = review_length
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +89,12 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
-
+    with torch.no_grad():
+        output = model.forward(data)
+        
+    output = output.to('cpu')
+    
+    result = np.round(output.numpy())
+    result = int(result)
+    
     return result
